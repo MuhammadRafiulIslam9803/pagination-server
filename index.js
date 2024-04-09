61017116d294a27e265bbe4c179d292b8d7231bd
Rafiul Islam <www.rafiulislam1998@gmail.com>
2024-04-10 05:15:16 +0600

    addede jwt and query email

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+
 
 //maddleware
 app.use(cors())
@@ -26,6 +29,20 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Invalid token' });
+        }
+        req.user = decoded;
+        next();
+    })
+}
 
 async function run() {
     const productCollection = client.db('productOfPagination').collection('products')
@@ -38,9 +55,9 @@ async function run() {
             const size = parseInt(req.query.size);
             const query = {}
             const cursor = productCollection.find(query)
-            const result = await cursor.skip(page*size).limit(size).toArray()
+            const result = await cursor.skip(page * size).limit(size).toArray()
             const count = await productCollection.estimatedDocumentCount()
-            res.send({result,count})
+            res.send({ result, count })
         })
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
@@ -51,21 +68,36 @@ async function run() {
             const result = await productCollection.deleteOne(query)
             res.send(result)
         })
+        // send jwt token 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
         //orders 
-        app.post('/orders' , async(req , res)=>{
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await ordersCollection.insertOne(order)
             res.send(result)
         })
-        app.get('/orders', async(req , res)=>{
-            const query = {}
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const user = req.user; // Access decoded payload from req.user
+            if (user.email !== req.query.email) {
+                return res.status(403).send({ message: 'Forbidden access' });
+            }
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
             const cursor = ordersCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
         })
-        app.delete('/orders/:id' , async(req , res)=>{
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await ordersCollection.deleteOne(query)
             res.send(result)
         })
